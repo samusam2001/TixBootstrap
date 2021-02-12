fb2d2d8348159597afa3097429a2df9083271d15
Nguyen <minhvn93@gmail.com>
2021-02-12 16:50:23 +0700

    xong carousel

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
-    var fixHeight = function() {
-      $('.navbar-nav').css(
-        'max-height',
-        document.documentElement.clientHeight - 150
-      );
-    };
-    fixHeight();
-    $(window).resize(function() {
-      fixHeight();
-    });
-    $('.navbar .navbar-toggler').on('click', function() {
-      fixHeight();
-    });
-    $('.navbar-toggler, .overlay').on('click', function() {
-      $('.mobileMenu, .overlay').toggleClass('open');
-    });
-  });
+//CAROUSEL
+$('.carousel .owl-carousel').owlCarousel({
+  margin: 0,
+  loop: true,
+  autoplay:true,
+  autoplayTimeout:5000,
+  autoplayHoverPause:true,
+  navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+  ],
+  navContainer: '.carousel .custom-nav',
+  responsive:{
+      0:{
+          items: 1
+      },
+  }
+});
